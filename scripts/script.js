fc86a4f5974f2fd197bf0076e9eab9c4f938d647
José Matheus <XxFULLDLCxX@outlook.com>
2023-04-07 10:39:22 -0300

    Melhorando a visualização dos cards, mudando a implementação das images e alterando nomes de variaveis

--- scripts/script.js
+++ scripts/script.js
@@ -9,15 +9,20 @@
 const cards = document.querySelector('.cards');
 const card = document.querySelector('.card');
 
+window.oncontextmenu = (event) => { 
+    event.preventDefault(); 
+    return false;
+  };
+
 cards.innerHTML = '';
 cards.classList.remove('hidden');
 
 const timer_content = document.querySelector('.timer-content');
 
 let cards_pressed = 0;
-let time_running = false;
+let timer_running = false;
 let timer_count = 0;
-
+`
 // A ordem importa. Não sei duplicar um array no JS. I'm so sorry.
 const images_front_face_names_duplicate = [
     'metalparrot.gif', 'metalparrot.gif',
@@ -28,32 +33,33 @@ const images_front_face_names_duplicate = [
     'revertitparrot.gif', 'revertitparrot.gif',
     'tripletsparrot.gif', 'tripletsparrot.gif',
 ];
-
-console.log(images_front_face_names_duplicate);
-
+`;
 const msg = "Quantas cartas quer jogar? (pares de 4 à 14) ";
-
-//function defineCard 
-
-//while (Number(prompt(msg)) % 2 === 0) {}
-
-// Geração de uma Quantidade de Cards.
-
 const number_cards = 4;
-const images_front_face_names_sliced = images_front_face_names_duplicate.slice(0, number_cards);
-const images_front_face_names_random = images_front_face_names_sliced.sort(
-    () => { return (Math.random() - 0.5); });
-console.log(images_front_face_names_random);
+
+
+const images = [];
+const sliced_random_images = [
+    'metalparrot.gif', 'fiestaparrot.gif', 'bobrossparrot.gif', 'explodyparrot.gif',
+    'unicornparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif',
+].sort(() => { return (Math.random() - 0.5); }).slice(0, number_cards / 2);
+
+for (let i = 0; i < sliced_random_images.length; i++) {
+    images.push(sliced_random_images[i])
+    images.push(sliced_random_images[i])
+}
+
+const random_images = images.sort(() => { return (Math.random() - 0.5); });
 
 for (let i = 0; i < number_cards; i++) {
-    card.querySelector('.front-face img').setAttribute('src', './assets/' + images_front_face_names_random[i]);
+    card.querySelector('.front-face img').setAttribute('src', './assets/' + random_images[i]);
     cards.innerHTML += card.outerHTML;
 }
 
 //Conômetro is here.
 //Essa sintaxe é muito bonita. Pena que deveria ser em camelCase.
 const start_timer = () => {
-    if (time_running) {
+    if (timer_running) {
         timer_count++;
         if (timer_count < 10) {
             timer_content.textContent = '0' + timer_count;
@@ -67,8 +73,8 @@ const start_timer = () => {
 function getCard(card_press) {
     // Controle do Frente e Verso
     // Lembrar de Adicionar . para classes
-    if (!time_running) {
-        time_running = true;
+    if (!timer_running) {
+        timer_running = true;
         start_timer();
     }
     if (!card_press.classList.contains('pressed') && !cards.classList.contains('block')) {
@@ -95,9 +101,10 @@ function getCard(card_press) {
             }
         }
         if (document.querySelectorAll('.freeze').length === number_cards) {
-            time_running = false;
-            setTimeout(() => { 
-                alert(`Você ganhou em ${cards_pressed} jogadas! A duração do jogo foi de ${timer_count} segundos!`); }, 100);
+            timer_running = false;
+            setTimeout(() => {
+                alert(`Você ganhou em ${cards_pressed} jogadas! A duração do jogo foi de ${timer_count} segundos!`);
+            }, 100);
         }
     }
 }
@@ -111,7 +118,7 @@ function getCard(card_press) {
 function switchBtn(btn) {
     btn.classList.toggle('on');
     btn.classList.toggle('off');
-    game_running = btn.classList.contains('on');
-    console.log(game_running, "game_running");
+    timer_running = btn.classList.contains('on');
+    console.log(timer_running, "game_running");
     start_timer();
 }

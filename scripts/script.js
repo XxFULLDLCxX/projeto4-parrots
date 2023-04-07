d1a166e6d1fa9c22a63fea57597dd488a9ad6930
José Matheus <XxFULLDLCxX@outlook.com>
2023-04-07 11:39:41 -0300

    Adicionando Comentários, Limpando um pouco o código, Reavaliando Metódologia

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,10 @@
 const cards = document.querySelector('.cards');
 const card = document.querySelector('.card');
 
-window.oncontextmenu = (event) => { 
-    event.preventDefault(); 
+window.oncontextmenu = (event) => {
+    event.preventDefault();
     return false;
-  };
+};
 
 cards.innerHTML = '';
 cards.classList.remove('hidden');
@@ -22,21 +22,9 @@ const timer_content = document.querySelector('.timer-content');
 let cards_pressed = 0;
 let timer_running = false;
 let timer_count = 0;
-`
-// A ordem importa. Não sei duplicar um array no JS. I'm so sorry.
-const images_front_face_names_duplicate = [
-    'metalparrot.gif', 'metalparrot.gif',
-    'fiestaparrot.gif', 'fiestaparrot.gif',
-    'bobrossparrot.gif', 'bobrossparrot.gif',
-    'explodyparrot.gif', 'explodyparrot.gif',
-    'unicornparrot.gif', 'unicornparrot.gif',
-    'revertitparrot.gif', 'revertitparrot.gif',
-    'tripletsparrot.gif', 'tripletsparrot.gif',
-];
-`;
+
 const msg = "Quantas cartas quer jogar? (pares de 4 à 14) ";
 const number_cards = 4;
-
 
 const images = [];
 const sliced_random_images = [
@@ -45,8 +33,8 @@ const sliced_random_images = [
 ].sort(() => { return (Math.random() - 0.5); }).slice(0, number_cards / 2);
 
 for (let i = 0; i < sliced_random_images.length; i++) {
-    images.push(sliced_random_images[i])
-    images.push(sliced_random_images[i])
+    images.push(sliced_random_images[i]);
+    images.push(sliced_random_images[i]);
 }
 
 const random_images = images.sort(() => { return (Math.random() - 0.5); });
@@ -56,8 +44,6 @@ for (let i = 0; i < number_cards; i++) {
     cards.innerHTML += card.outerHTML;
 }
 
-//Conômetro is here.
-//Essa sintaxe é muito bonita. Pena que deveria ser em camelCase.
 const start_timer = () => {
     if (timer_running) {
         timer_count++;
@@ -70,34 +56,53 @@ const start_timer = () => {
     }
 };
 
+
 function getCard(card_press) {
-    // Controle do Frente e Verso
-    // Lembrar de Adicionar . para classes
+    `
+    O getCard é chamado, sempre que um card é pressionado.
+    ... Descrição do Fluxo de Execução (Análise do Metódo).
+    Comece o Timer se ele não estiver rodando.
+    Casos de Desconsideração de Click: 
+        Se o card a ser pressionado já foi pressionado.
+        Se a sessão de cards está bloqueada (a animação está acontencendo).
+    Atualiza o Contador de Jogadas.
+    Quando Dois Cards são Virados:
+        Verifica se eles tem 'img src' diferentes:
+            Se sim, bloqueia a sessão de cards até eles serem, automaticamente, Desvirados.
+            Se não, ficaram como estão e serão congelados no tempo (poético).
+    Condição de finalização:
+        Se o Valor Total de Cards Congelados é exatamente igual a Quantidade de Cards na Partida.
+    Finalização de Partida:
+        Espere um pouco a animação terminar e chame o Alerta de Vitória.
+        Limpe a Sessão de Cards
+        Reset o Timer
+        Reset o Contandor de Jogadas.
+        Chame a Função Responsável por Começar o Jogo.
+    `;
     if (!timer_running) {
         timer_running = true;
         start_timer();
     }
     if (!card_press.classList.contains('pressed') && !cards.classList.contains('block')) {
-        card_press.classList.toggle('pressed');
-
+        card_press.classList.add('pressed');
         cards_pressed++;
-
+        // Seleciona os cards pressionados, mas não os que já estão congelados.
         const card_pressed = document.querySelectorAll('.pressed:not(.freeze)');
 
-        const img_front_face = document.querySelectorAll('.pressed:not(.freeze) .front-face img');
+        const images_front_face = document.querySelectorAll('.pressed:not(.freeze) .front-face img');
         if (card_pressed.length % 2 === 0) {
-            cards.classList.add('block');
-            console.log(img_front_face[0].getAttribute('src'), img_front_face[1].getAttribute('src'));
-            if (img_front_face[0].getAttribute('src') !== img_front_face[1].getAttribute('src')) {
+            if (images_front_face[0].getAttribute('src') !== images_front_face[1].getAttribute('src')) {
+                // Inicia o Processo de Desvirar os Cards com Imagens Diferentes.
+                cards.classList.add('block');
                 setTimeout(() => {
-                    card_pressed[0].classList.toggle('pressed');
-                    card_pressed[1].classList.toggle('pressed');
+                    card_pressed[0].classList.remove('pressed');
+                    card_pressed[1].classList.remove('pressed');
                     cards.classList.remove('block');
                 }, 1000);
             } else {
+                // Congela os Cards Virados com Imagens Iguais.
                 card_pressed[0].classList.add('freeze');
                 card_pressed[1].classList.add('freeze');
-                cards.classList.remove('block');
             }
         }
         if (document.querySelectorAll('.freeze').length === number_cards) {
